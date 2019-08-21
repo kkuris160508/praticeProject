a1010419823704ac8f4ee61db593baa3276a195b
kkuris160508 <kkuris160508@gmail.com>
2019-08-21 21:00:32 +0900

    commit

--- test2.js
+++ test2.js
@@ -3,6 +3,5 @@
     console.log();
     jakl;sdjkl;asdf;klajsdf;kljasd;kfjas;ldfj;alksdf;lkasjflkasjdfkjdkfjdkfjkdjf
     </script>
-    jasdklfja;sklfklasdf;klasjflkajsdflkjasdlfkjasdfkljasdlkfjasdlfjasdf nvldienfie
+    jasdklfja;sklfklasdf;klasjflkajsdflkjasdlfkjasdfkljasdlkfjasdlfjasdf
 
-    
